0247c76585f973f6969990baab80011e34de8afe
DenisIvanov26 <denis.ivanov26@gmail.com>
2023-11-03 11:17:44 -0500

    simple block

--- packages/providers/lib/formatter.js
+++ packages/providers/lib/formatter.js
@@ -330,7 +330,7 @@ var Formatter = /** @class */ (function () {
         }
         return (0, bytes_1.hexZeroPad)(value, 32);
     };
-    Formatter.prototype._block = function (value, format, context) {
+    Formatter.prototype._block = function (value, format, context, simplify) {
         if (value.author != null && value.miner == null) {
             value.miner = value.author;
         }
@@ -339,23 +339,24 @@ var Formatter = /** @class */ (function () {
         var result = Formatter.check(format, value);
         result._difficulty = ((difficulty == null) ? null : difficulty);
         if (context) {
-            return this.contextBlock(result, context);
+            return this.contextBlock(result, context, simplify);
         }
         return result;
     };
-    Formatter.prototype.block = function (value, context) {
-        return this._block(value, this.formats.block, context);
+    Formatter.prototype.block = function (value, context, simplify) {
+        return this._block(value, this.formats.block, context, simplify);
     };
     Formatter.prototype.blockWithTransactions = function (value) {
         return this._block(value, this.formats.blockWithTransactions);
     };
-    Formatter.prototype.contextBlock = function (value, context) {
+    Formatter.prototype.contextBlock = function (value, context, simplify) {
+        if (simplify === void 0) { simplify = false; }
         var contextBlock = {
-            number: value.number,
+            number: simplify ? value.number[2] : value.number,
             transactions: value.transactions,
             hash: value.hash,
-            parentHash: value.parentHash,
-            parentEntropy: value.parentEntropy,
+            parentHash: simplify ? value.parentHash[2] : value.parentHash,
+            parentEntropy: simplify ? value.parentEntropy[2] : value.parentEntropy,
             extTransactions: value.extTransactions,
             timestamp: value.timestamp,
             nonce: value.nonce,
@@ -371,10 +372,10 @@ var Formatter = /** @class */ (function () {
             extRollupRoot: value.extRollupRoot,
             extTransactionsRoot: value.extTransactionsRoot,
             location: value.location,
-            manifestHash: value.manifestHash,
+            manifestHash: simplify ? value.manifestHash[2] : value.manifestHash,
             mixHash: value.mixHash,
             order: value.order,
-            parentDeltaS: value.parentDeltaS,
+            parentDeltaS: simplify ? value.parentDeltaS[2] : value.parentDeltaS,
             sha3Uncles: value.sha3Uncles,
             size: value.size,
             uncles: value.uncles,
